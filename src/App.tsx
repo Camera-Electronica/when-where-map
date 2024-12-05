a53fad977f8068f18ef81d106e66f6036cba73c0
Ilia Pchelarov <i.pchelarov@gmail.com>
2024-12-05 10:06:54 +0200

    Add deployment with gh-pages.

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div title="Main" className="App">
       <APIProvider apiKey={'AIzaSyBACtK57ZxOTew4nKxADL7dvsdEyMHiaZ4'} onLoad={() => console.log('Maps API has loaded.')}>
-        <h3>Events Map</h3>
+        <h3>When-Where Events</h3>
         <EventMap />
       </APIProvider>
     </div>
